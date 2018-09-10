97a43f4c2d69ea4e6ff4cebac3c1f16237626e84
wislanlins <wislanecko@gmail.com>
2018-09-10 16:44:15 -0300

    cadastro

---
+++ Clinica/Cadastro.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Clinica.Model;
+
+namespace Clinica
+{
+    public partial class Cadastro : System.Web.UI.Page
+    {
+        Paciente[] Banco = new Paciente[] {
+            
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string Pacientes(string cpf)
+        {
+            return "just a test";
+        }
+    }
+}
