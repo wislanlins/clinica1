254837ed1d9adeb6ea5020eb8e95b00afc88b980
Cristiano Alves da Silva Junior <cristianoalvesjr@gmail.com>
2018-09-18 14:56:43 -0300

    Adding tests for mask functions

--- Clinica/Clinica.Tests/Views/Home/js/main.test.js
+++ Clinica/Clinica.Tests/Views/Home/js/main.test.js
@@ -7,7 +7,7 @@ describe('Validações', function() {
                 done(new Error("CPF '00000000000' deveria ser inválido"));
             }
         });
-        
+
         it('Deve validar um CPF válido mesmo que não esteja ligado a ninguém', function(done) {
             if (validarCpf("31066927073")) {
                 done();
@@ -16,7 +16,7 @@ describe('Validações', function() {
             }
         });
     });
-    
+
     describe('Datas', function() {
         it('Não deve aceitar uma data em um formato errado.', function(done) {
             if (validarDataDeNascimento("25/05") === false) {
@@ -25,7 +25,7 @@ describe('Validações', function() {
                 done(new Error("Data '25/05' deveria ser um formato inválido"));
             }
         });
-        
+
         it('Não deve passar uma data inexistente.', function(done){
             if (validarDataDeNascimento("30/02/2003") === false) {
                 done();
@@ -33,7 +33,7 @@ describe('Validações', function() {
                 done(new Error("Dia 30/02 não existe"));
             }
         });
-        
+
         it('Deve aceitar anos bissextos.', function(done){
             if (validarDataDeNascimento("29/02/2012") === true) {
                 done();
@@ -42,7 +42,7 @@ describe('Validações', function() {
             }
         });
     });
-    
+
     describe('Peso e altura', function() {
         it('Não pode aceitar "abc" como altura ou peso', function(done) {
             let alturaValida = validarAltura("abc");
@@ -57,7 +57,7 @@ describe('Validações', function() {
                 done();
             }
         }); 
-        
+
         it('Não pode aceitar valores negativos como medida', function(done) {
             let entrada = "-3";
             let alturaValida = validarAltura(entrada);
@@ -72,7 +72,7 @@ describe('Validações', function() {
                 done();
             }
         });    
-        
+
         it('Não pode receber unidades de medida na entrada', function(done) {
             let alturaValida = validarAltura("175cm");
             let pesoValido =  validarPeso("65kg");
@@ -87,6 +87,48 @@ describe('Validações', function() {
             }
         }); 
     });
-    
-    /* TODO Write tests for masks */
+});
+
+describe("Máscaras", function(){
+    describe("CPF", function() {
+        it("Consegue mascarar um CPF", function(done) {
+            let masked = mCPF("12345678912"); 
+            if (masked === "123.456.789-12") {
+                done();
+            } else {
+                done(new Error("CPF mascarado de forma errada."));
+            }
+        });
+    });
+
+    describe("CEP", function() {
+        it("Consegue mascarar um CEP", function(done) {
+            let masked = mCEP("71000000"); 
+            if (masked === "71000-000") {
+                done();
+            } else {
+                done(new Error("CEP mascarado de forma errada."));
+            }
+        });
+    });
+
+    describe("Telefone", function() {
+        it("Consegue mascarar um telefone fixo", function(done) {
+            let masked = mTel("6232149876"); 
+            if (masked === "(62) 3214-9876") {
+                done();
+            } else {
+                done(new Error("Telefone fixo mascarado de forma errada: " + masked));
+            }
+        });
+        
+        it("Consegue mascarar um telefone celular", function(done) {
+            let masked = mTel("62982149876"); 
+            if (masked === "(62) 98214-9876") {
+                done();
+            } else {
+                done(new Error("Telefone celular mascarado de forma errada: " + masked));
+            }
+        });
+    });
 });
